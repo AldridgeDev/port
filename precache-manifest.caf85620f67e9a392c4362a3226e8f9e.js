f9a258b59fa9f83bf299ad5874b47594b1c56a66
Antonio Aldridge <aldridge.developer@gmail.com>
2024-12-20 12:10:29 -0500

    Updates

---
+++ precache-manifest.caf85620f67e9a392c4362a3226e8f9e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28de06161dc8ca9b12a12735069af63b",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "97dafc37605a3297207c",
+    "url": "/port/static/css/2.5290c630.chunk.css"
+  },
+  {
+    "revision": "32f3c0dbd15b76752908",
+    "url": "/port/static/css/main.a9382249.chunk.css"
+  },
+  {
+    "revision": "97dafc37605a3297207c",
+    "url": "/port/static/js/2.c69f96ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/port/static/js/2.c69f96ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f3c0dbd15b76752908",
+    "url": "/port/static/js/main.44c1c2dd.chunk.js"
+  },
+  {
+    "revision": "bbbea0931ba6940697c5",
+    "url": "/port/static/js/runtime-main.ab55f0a1.js"
+  },
+  {
+    "revision": "ab129dd2f0a3e621dd6a6d82eca69ee2",
+    "url": "/port/static/media/abot.ab129dd2.svg"
+  },
+  {
+    "revision": "7dfac08d683b81d4755c4f28ae936f2e",
+    "url": "/port/static/media/bg.7dfac08d.jpg"
+  },
+  {
+    "revision": "fb5ec271f3c8677751313a157d4162a0",
+    "url": "/port/static/media/fot.fb5ec271.svg"
+  },
+  {
+    "revision": "3227854943a224f307bd289dd284d52d",
+    "url": "/port/static/media/log.32278549.svg"
+  },
+  {
+    "revision": "b5012fa9723f3e06ab13841e7ed4ad48",
+    "url": "/port/static/media/m1.b5012fa9.svg"
+  },
+  {
+    "revision": "c34dcd63e13ffb060cb210bf83cc8f87",
+    "url": "/port/static/media/mb.c34dcd63.svg"
+  }
+]);
